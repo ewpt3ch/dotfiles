a28f3f35f133d5ca64b36950bd5ac9daf50c0ff9
Eric Phillips <ewpt3ch@gmail.com>
2015-04-05 23:28:53 -0600

    set firefox to open on current tag(s)

--- dwm.config.h
+++ dwm.config.h
@@ -22,7 +22,7 @@ static const char *tags[] = { "term", "work", "www", "dev" };
 static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            True,        -1 },
-	{ "Firefox",  NULL,       NULL,       1,            False,       -1 },
+	{ "Firefox",  NULL,       NULL,       0,            False,       -1 },
 };
 
 /* layout(s) */
