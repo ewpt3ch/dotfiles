6dd3017b429f421b36d62af0e65efc80a8e96fb2
Eric Phillips <ewpt3ch@gmail.com>
2015-06-26 18:41:52 -0600

    pulled dwm config from yoga2 into master

--- dwm.config.h
+++ dwm.config.h
@@ -3,25 +3,33 @@
 #include <X11/XF86keysym.h> /*Needed for mulitmedia keys*/
 
 /* appearance */
-static const char font[]            = "Ohsnap.Icons:size=12";
-static const char normbordercolor[] = "#444444";
-static const char normbgcolor[]     = "#222222";
-static const char normfgcolor[]     = "#bbbbbb";
-static const char selbordercolor[]  = "#005577";
-static const char selbgcolor[]      = "#005577";
-static const char selfgcolor[]      = "#eeeeee";
+static const char font[]  =                     "Inconsolataicon-12";
+
+#define NUMCOLORS 9 
+static const char colors[NUMCOLORS][ColLast][9] = {
+  // border foreground background
+  { "#212121", "#00FFFF", "#2F4F4F" }, // 0 = normal
+  { "#696969", "#ADFF2F", "#2F4F4F" }, // 1 = selected
+  { "#212121", "#F8F8FF", "#C71585" }, // 2 = urgent 
+  { "#212121", "#90EE90", "#778899" }, // 3 = important 
+  { "#212121", "#FFFF00", "#2F4F4F" }, // 4 = yellow
+  { "#212121", "#00BFFF", "#2F4F4F" }, // 5 = blue
+  { "#212121", "#00FFFF", "#2F4F4F" }, // 6 = cyan
+  { "#212121", "#FF00FF", "#2F4F4F" }, // 7 = magenta 
+  { "#212121", "#F5F5F5", "#2F4F4F" }, // 8 = grey
+  };
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const Bool showbar           = True;     /* False means no bar */
 static const Bool topbar            = True;     /* False means bottom bar */
 
 /* tagging */
-static const char *tags[] = { "term", "coms", "www", "dev", "work", "6", "7", "8", "9" };
+static const char *tags[] = { "\u01A0", "\u01a5", "\u01A1 ", "\u01A2", "\u01A3 " };
 
 static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            True,        -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       False,       -1 },
+	{ "Firefox",  NULL,       NULL,       0,       False,       -1 },
 };
 
 /* layout(s) */
@@ -39,8 +47,8 @@ static const Layout layouts[] = {
 /* key definitions */
 #define MODKEY Mod1Mask
 #define TAGKEYS(KEY,TAG) \
-	{ MODKEY,                       KEY,      toggleview,           {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask,           KEY,      view,     {.ui = 1 << TAG} }, \
+	{ MODKEY,                       KEY,      toggleview,     {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask,           KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
@@ -48,7 +56,7 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static const char *dmenucmd[] = { "dmenu_run", "-fn", "Inconsolata-9", "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
+static const char  *dmenucmd[] = { "dmenu_run", "-fn", font, "-nb", colors[0][ColBG], "-nf", colors[0][ColFG], "-sb", colors[1][ColBG], "-sf", colors[1][ColFG], NULL };
 static const char *termcmd[]  = { "termite", NULL };
 /*functions for volume control*/
 static const char *upvol[] = { "amixer", "sset", "Master", "1%+", NULL};
@@ -65,7 +73,9 @@ static Key keys[] = {
   { 0,              XF86XK_AudioLowerVolume, spawn,          {.v = downvol} },
   { 0,              XF86XK_AudioMute,        spawn,          {.v = mute } },
   { MODKEY,                       XK_u,      spawn,          {.v = cmusplaypause} },
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+  { MODKEY,                       XK_bracketright,     spawn,          {.v = cmusnext} },
+  { MODKEY,                       XK_bracketleft,      spawn,          {.v = cmusprev} },
+  { MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
@@ -90,13 +100,9 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2) 
+	TAGKEYS(                        XK_3,                      2)
 	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
+  TAGKEYS(                        XK_5,                      4)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 };
 
