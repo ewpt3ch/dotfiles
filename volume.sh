51838a8ab4c8e92e7625a0af6537b4c12f13326e
Eric Phillips <eric@ewpt3ch.com>
2016-06-12 22:33:15 -0600

    removed redundant script

--- volume.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mute=`amixer get Master | grep "Front Left:" | awk '{print $6}'`
-if [ $mute == "[on]" ]
-then
-  vol=`amixer get Master | grep "Front Left:" | awk '{print $5}' | tr -d '[]'`
-  echo "vol" $vol
-else
-  echo "mute"
-fi
